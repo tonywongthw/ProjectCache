681f5b94aa23525cfca0e6aed7fa21fd753c39c4
Tony <66058518+tonywongthw@users.noreply.github.com>
2022-09-11 11:52:38 +1000

    Update to ImgName and isLike

--- HttpGetRandomImageWebAPI/Models/TodoItem.cs
+++ HttpGetRandomImageWebAPI/Models/TodoItem.cs
@@ -3,7 +3,7 @@
     public class TodoItem
     {
         public long Id { get; set; }
-        public string? Name { get; set; }
-        public bool IsComplete { get; set; }
+        public string? ImgName { get; set; }
+        public bool IsLike { get; set; }
     }
 }
