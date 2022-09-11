5ce5875e270d543c5164a59c2a1a200c8ad535fa
Tony <66058518+tonywongthw@users.noreply.github.com>
2022-09-11 12:20:40 +1000

    api added userID

--- HttpGetRandomImageWebAPI/Models/Item.cs
+++ HttpGetRandomImageWebAPI/Models/Item.cs
@@ -3,7 +3,9 @@
     public class Item
     {
         public long Id { get; set; }
-        public string? ImgName { get; set; }
-        public bool? IsLike { get; set; }
+
+        public string UserID { get; set; }
+        public string ImgName { get; set; }
+        public bool IsLike { get; set; }
     }
 }
